546e4aa20d652a2751e26d06181ae52ea0333307
时思芫 <SteeleShi@shisiyuandeMBP.fritz.box>
2020-08-14 12:34:46 +0200

    first commit

--- projects/capstone/heroku_sample/starter/models.py
+++ projects/capstone/heroku_sample/starter/models.py
@@ -1,8 +1,13 @@
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+import os
 
-database_path = os.environ['DATABASE_URL']
+# database_path = os.environ['DATABASE_URL']
+database_path = os.environ.get('DATABASE_URL')
+if not database_path:
+    database_name = "example"
+    database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
