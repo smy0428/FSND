f028e25f4d14218ec0c06411aa87465a03592716
时思芫 <SteeleShi@shisiyuandeMBP.fritz.box>
2020-08-16 00:06:13 +0200

    try to push with Head:master

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,3 +1,4 @@
 DATABASE_URL=postgres://zkbukaqijybbny:e2839af1d864857cd374f6d897548fd36fa9300ce6f7b27ed342cdca6f177fef@ec2-54-197-254-117.compute-1.amazonaws.com:5432/d30tklik4dlrve
+
 AUTH0_DOMAIN=peter-coffee-shop.us.auth0.com
 API_AUDIENCE=casting_agency
